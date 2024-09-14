8fc04762326b9b27c2542bca4d75e1e5f401ddd2
HoaMabu <luonghoa121002@gmail.com>
2024-09-14 22:48:02 +0700

    up

--- extend.php
+++ extend.php
@@ -19,19 +19,7 @@ return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__ . '/js/dist/forum.js')
         ->css(__DIR__ . '/css/forum.css')
-        ->route('/sao-ke-tien-ung-ho-bao-yagi', 'hoa1210/hoamabuu')
-        ->content(function (Document $document, Request $request) {
-            // foreach ($document->head as $key => $tag) {
-            //     if (strpos($tag, 'property="og:') !== false) {
-            //         unset($document->head[$key]);
-            //     }
-            // }
-            // $document->head[] = '<meta property="og:title" content="Check var sao kê Mặt trận tổ quốc Việt Nam ">
-            // <meta property="og:image" content="https://imgur.com/UAscxDr">
-            // <meta property="og:description" content="Tìm kiếm dữ liệu sao kê từ 12000 PDF của Mặt Trận Tổ Quốc Việt Nam">
-            // <meta property="og:url" content="https://hotroit.org">
-            // <meta property="og:type" content="website">';
-        }),
+        ->route('/sao-ke-tien-ung-ho-bao-yagi', 'hoa1210/hoamabuu'),
 
     (new Extend\Frontend('admin'))
         ->js(__DIR__ . '/js/dist/admin.js'),
