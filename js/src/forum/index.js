cd4c12e9b592c2d39b723fdfe83f94ca35909aa4
HoaMabu <luonghoa121002@gmail.com>
2024-09-18 10:32:32 +0700

    up

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,6 +3,8 @@ import StatementPage from './components/StatementPage';
 import LinkButton from 'flarum/components/LinkButton';
 import { extend } from 'flarum/common/extend';
 import IndexPage from 'flarum/components/IndexPage';
+import UserCard from 'flarum/components/UserCard';
+import Button from 'flarum/common/components/Button';
 
 
 app.initializers.add('hoa1210/hoamabu', () => {
@@ -20,4 +22,20 @@ app.initializers.add('hoa1210/hoamabu', () => {
       100
     );
   });
+
+  extend(UserCard.prototype, 'infoItems', function (items) {
+    items.add(
+      'profileButton',
+      Button.component(
+        {
+          className: 'Button Button--primary ',
+          onclick: () => {
+            jqac.arrowchat.chatWith(app.current.data.user.data.id);
+          },
+        },
+        'Liên hệ ngay'
+      )
+    );
+  });
+
 });
